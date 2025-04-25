1c3f22306fc3f8a4889c5be89252af49cedb36d2
ramavathar348 <ramavatharyadavkanneboina@gmail.com>
2025-04-25 04:46:09 +0000

    initial commit bdd

---
+++ Project/src/test/java/utils/LoggerHandler.java
@@ -0,0 +1,93 @@
+package utils;
+ 
+import java.io.File;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+ 
+/*
+ * Class Name: LoggerHandler
+ * Author Name: Sharmila Abdul
+ * Description: Handles logging operations using Log4j2, ensuring logs directory exists.
+ */
+public class LoggerHandler {
+ 
+    private static final Logger log = LogManager.getLogger(LoggerHandler.class);
+ 
+    /*
+     * Constructor Name: LoggerHandler
+     * Author Name: Sharmila Abdul
+     * Description: Initializes the LoggerHandler and ensures the "logs" directory exists.
+     */
+    public LoggerHandler() {
+        File logsDir = new File("logs");
+        if (!logsDir.exists()) {
+            logsDir.mkdirs();
+        }
+    }
+ 
+    /*
+     * Method Name: logTrace
+     * Author Name: Sharmila Abdul
+     * Description: Logs a trace level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logTrace(String msg) {
+        log.trace(msg);
+    }
+ 
+    /*
+     * Method Name: logDebug
+     * Author Name: Sharmila Abdul
+     * Description: Logs a debug level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logDebug(String msg) {
+        log.debug(msg);
+    }
+ 
+    /*
+     * Method Name: logInfo
+     * Author Name: Sharmila Abdul
+     * Description: Logs an info level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logInfo(String msg) {
+        log.info(msg);
+    }
+ 
+    /*
+     * Method Name: logWarn
+     * Author Name: Sharmila Abdul
+     * Description: Logs a warn level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logWarn(String msg) {
+        log.warn(msg);
+    }
+ 
+    /*
+     * Method Name: logError
+     * Author Name: Sharmila Abdul
+     * Description: Logs an error level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logError(String msg) {
+        log.error(msg);
+    }
+ 
+    /*
+     * Method Name: logFatal
+     * Author Name: Sharmila Abdul
+     * Description: Logs a fatal level message.
+     * Return type: void
+     * Param List: String msg - The message to log.
+     */
+    public void logFatal(String msg) {
+        log.fatal(msg);
+    }
+}
---
+++ Project/src/test/java/utils/Screenshot.java
@@ -0,0 +1,31 @@
+package utils;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+ 
+public class Screenshot extends Base {
+ 
+     /*
+         * Method Name: takeScreenshot
+         * Author Name: Venkatanarayana
+         * Description: This method captures a screenshot, names it with a timestamp, and saves it to a specified directory.
+         * Return Type: void
+         * Parameter List: String filename - The base name for the screenshot file.
+         */
+    public static void takeScreenshot(String filename) {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String name = filename + timestamp + ".png";
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir") + "/screenshots/";
+        try {
+            FileUtils.copyFile(screenshot, new File(path + name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
